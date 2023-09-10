817a1be258c4189db495338e04f2bc8c6213be85
SikozuSarean <antonio.celestino@.com>
2023-09-10 15:58:47 +0300

    qq

--- app/routers/user.py
+++ app/routers/user.py
@@ -7,8 +7,8 @@ from ..database import get_db
 router = APIRouter(
     prefix="/users",
     tags=['Users']
-
 )
+
 @router.post("/", status_code=status.HTTP_201_CREATED,response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     #hash the pwd - user..password
